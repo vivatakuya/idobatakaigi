4487b39784d76a53153a23ebaf88b2f4a7e7f946
大沢卓也 <oosawatakuya@oosawatakuyanoMacBook-Air.local>
2021-01-30 07:00:24 +0900

    　日本語で名前保存

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SignIn from './SignIn';
+import Main from "./Main";
 
+import SignIn from "./SignIn";
 
 export default () => {
-  const [name,setName] =useState("");
-  console.log({name});
-  return <SignIn setName={setName}/>;
+  const [name, setName] = useState("");
+  console.log({ name });
+
+  if (name === "") {
+    return <SignIn setName={setName} />;
+  } else {
+    return <Main name={name} />;
+  }
 };
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -44,7 +44,7 @@ export default function SignIn({setName}) {
   const classes = useStyles();
   const [disabled, setDisabled]=useState(true);
   const[string,setString]= useState("");
-  console.log({disabled,string});
+  const[isComposed,setIsComposed]=useState(false);
 
   useEffect(()=>{
       const disabled = string === ""
@@ -70,6 +70,17 @@ export default function SignIn({setName}) {
             name="name"
             autoFocus
             onChange ={(e)=>setString(e.target.value)}
+            onKeyDown ={(e)=>{
+              
+            if (isComposed) return
+
+              if(e.key === 'Enter'){
+                setName(e.target.value);
+              e.preventDefault();
+              }
+            }}
+            onCompositionStart={()=> setIsComposed(true)}
+            onCompositionEnd={()=> setIsComposed(false)}
           />
           
          
