b3a25ee3e4e17f1940aaa9312c6a808750250eda
大沢卓也 <oosawatakuya@oosawatakuyanoMacBook-Air.local>
2021-01-31 08:16:10 +0900

    generate-avatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {Avatar, Grid } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Avatar, Grid } from "@material-ui/core";
 
+import { gravatarPath } from "../gravatar";
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+    margin: "26px",
+  },
+});
 
- const useStyles =makeStyles({
-    root:{
-        gridRow:2,
-        margin: '26px',
+const MessageInputField = ({ name }) => {
+  const classes = useStyles();
+  const avatarPath = gravatarPath(name);
 
-    }
-})
-
-const MessageInputField =()=>{
-    const classes =useStyles();
-    return(<div className={classes.root}>
-<Grid container>
-    <Grid xs={1}>
-        <Avatar />
-    </Grid>
-    <Grid xs={10}>
-        入力
-    </Grid>
-    <Grid ws={1}>
-        ボタン
-    </Grid>
-</Grid>
-    </div>)
-}
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar src={avatarPath} />
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid ws={1}>ボタン</Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default MessageInputField;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
- const useStyles =makeStyles({
-    root:{
-        gridRow:1,
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,
+  },
+});
 
-    }
-})
-
-const MessageList =()=>{
-    const classes =useStyles();
-    return<div className={classes.root}>MessageList</div>
-}
+const MessageList = () => {
+  const classes = useStyles();
+  return <div className={classes.root}>MessageList</div>;
+};
 
 export default MessageList;
