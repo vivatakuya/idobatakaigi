63c2975bfa4d7ccf02a070f1da6cd935d0f15ef7
大沢卓也 <oosawatakuya@oosawatakuyanoMacBook-Air.local>
2021-01-31 14:53:28 +0900

    message-field-component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,28 @@
-import React from  'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import MessageInputField from './MessageInputField';
-import MessageList from './MessageList';
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
 
 const useStyles = makeStyles({
-    root:{
-        display: 'grid',
-        height: '100vh',
-        gridTemplateRows: '1fr auto',
-    }
-
+  root: {
+    display: "grid",
+    height: "100vh",
+    gridTemplateRows: "1fr auto",
+  },
 });
 
-const Main =({name})=>{
-    const classes =useStyles();
+const Main = ({ name }) => {
+  const classes = useStyles();
 
-       return(
-
-       <div className={classes.root}>
-           <>
-           <MessageList />
-           <MessageInputField name={name}/>
-           </>
-       </div>
-   )
-   
-}
+  return (
+    <div className={classes.root}>
+      <>
+        <MessageList />
+        <MessageInputField name={name} />
+      </>
+    </div>
+  );
+};
 
 export default Main;
