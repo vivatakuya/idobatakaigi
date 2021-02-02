be56847ff619ab7471814082ba4f96e1b8f80a08
大沢卓也 <oosawatakuya@oosawatakuyanoMacBook-Air.local>
2021-02-03 04:57:19 +0900

    submit button

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -4,6 +4,7 @@ import { Avatar, Grid } from "@material-ui/core";
 
 import { gravatarPath } from "../gravatar";
 import MessageField from './MessageField';
+import MessageSubmitButton from './MessageSubmitButton';
 
 const useStyles = makeStyles({
   root: {
@@ -26,7 +27,9 @@ const MessageInputField = ({ name }) => {
         <Grid item xs={10}>
           <MessageField name={name} setText={setText} text={text} />
         </Grid>
-        <Grid item xs={1}>ボタン</Grid>
+        <Grid item xs={1}>
+          <MessageSubmitButton name={name} setText={setText} text={text} />
+        </Grid>
       </Grid>
     </div>
   );
